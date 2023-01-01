bb97db1e6e675bdc87bcf6733fd515f00cac0385
felipeLeao18 <flpleao1807@gmail.com>
2023-01-01 16:46:34 -0300

    refactor: rename input folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from 'next/image'
 import styles from '../../styles/home.module.scss'
 import logoImg from '../../public/logo.svg'
-import { Input } from "../components/ui/input";
+import { Input } from "../components/ui/Input";
 
 export default function Home() {
   return (
