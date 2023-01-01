0cd42a1186a578a9e707c3d9d8de754d40cabdf7
felipeLeao18 <flpleao1807@gmail.com>
2023-01-01 18:33:55 -0300

    feat: add button to login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../../styles/home.module.scss'
 import logoImg from '../../public/logo.svg'
 import { Input } from '../components/ui/Input'
+import { Button } from '../components/ui/Button'
 
 export default function Home () {
   return (
@@ -22,10 +23,15 @@ export default function Home () {
           <form>
             <Input placeholder="Digite seu email" type="text"
               />
-
             <Input placeholder="Digite sua senha"
               type="password"
               />
+            <Button
+              type="submit"
+              loading={true}
+            >
+            login
+            </Button>
           </form>
         </div>
       </div>
