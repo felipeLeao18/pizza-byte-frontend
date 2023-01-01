1c8263d876d4a9f64dd1cb9219129e4fb565bb30
felipeLeao18 <flpleao1807@gmail.com>
2023-01-01 16:40:23 -0300

    feat: add input and text area components

---
+++ src/components/ui/input/index.tsx
@@ -0,0 +1,18 @@
+import styles from './styles.module.scss'
+import { InputHTMLAttributes, TextareaHTMLAttributes } from 'react'
+
+type IInputProps = InputHTMLAttributes<HTMLInputElement>
+
+type ITextAreaProps = TextareaHTMLAttributes<HTMLTextAreaElement>
+
+export function Input ({...rest}: IInputProps){
+  return (
+    <input className={styles.input} {...rest}/>
+  )
+}
+
+export function TextArea({...rest}: ITextAreaProps){
+  return (
+    <textarea className={styles.input} {...rest}></textarea>
+  )
+}
