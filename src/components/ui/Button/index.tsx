73c075cdad5985c76dc0b00704a46f9c1d9fcd61
felipeLeao18 <flpleao1807@gmail.com>
2023-01-01 18:33:40 -0300

    feat: add button style and interface

---
+++ src/components/ui/Button/index.tsx
@@ -0,0 +1,28 @@
+import styles from './styles.module.scss'
+import { ReactNode, ButtonHTMLAttributes } from 'react'
+
+import { FaSpinner } from 'react-icons/fa'
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  loading?: boolean
+  children: ReactNode
+}
+export function Button ({ loading, children, ...rest }: IButtonProps) {
+  return (
+    <button
+    className={styles.button}
+    disabled={loading}
+    {...rest}
+    >
+      {
+      (loading ?? false)
+        ? (<FaSpinner color="#FFF" size={16}/>)
+        : (
+
+      <a className={styles.buttonText}>
+        {children}
+      </a>
+          )}
+    </button>
+
+  )
+}
