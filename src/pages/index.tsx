c72fff7f577aa85c0c12da2d5b6cf0cc602f3392
felipeLeao18 <flpleao1807@gmail.com>
2023-01-01 16:40:48 -0300

    feat: add home page form to login

---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import Image from 'next/image'
+import styles from '../../styles/home.module.scss'
+import logoImg from '../../public/logo.svg'
+import { Input } from "../components/ui/input";
+
+export default function Home() {
+  return (
+    <>
+    <div>
+        <Head>
+          <title>
+            Login
+          </title>
+        </Head>
+    </div>
+      <div className={styles.containerCenter}>
+
+        <Image src={logoImg} alt="logo"/>
+
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu email" type="text"
+              />
+
+            <Input placeholder="Digite sua senha" 
+              type="password"
+              />
+          </form>
+        </div>
+      </div>
+      </>
+  )
+}
