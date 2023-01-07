3c34453d2671117fd8712d8414b1b4d4fb87d881
felipeLeao18 <flpleao1807@gmail.com>
2023-01-07 13:48:20 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,29 @@ import logoImg from '../../public/logo.svg'
 import Link from 'next/link'
 import { Input } from '@/src/components/ui/Input'
 import { Button } from '@/src/components/ui/Button'
+import { FormEvent, useContext, useState } from 'react'
+import { AuthContext } from '@/src/contexts/AuthContext'
+import { canSSRGuest } from '../utils/canSSRGuest'
+import { toast } from 'react-toastify'
 
 export default function Home () {
+  const { signIn } = useContext(AuthContext)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  async function handleLogin (event: FormEvent) {
+    event.preventDefault()
+
+    if (email === '' || (password === '')) {
+      toast.error('Enter email and password')
+    }
+
+    setLoading(true)
+    await signIn({ email, password })
+    setLoading(false)
+  }
   return (
     <>
     <div>
@@ -21,15 +42,21 @@ export default function Home () {
         <Image src={logoImg} alt="logo"/>
 
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input placeholder="Digite seu email" type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               />
             <Input placeholder="Digite sua senha"
               type="password"
+              value={password}
+
+              onChange={(e) => setPassword(e.target.value)}
               />
             <Button
               type="submit"
-              loading={false}
+
+              loading = {loading}
             >
             login
             </Button>
@@ -44,3 +71,9 @@ export default function Home () {
       </>
   )
 }
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+})
