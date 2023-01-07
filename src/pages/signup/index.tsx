a6da5f359218eb4046166ffc4c0aca9b3bc3519c
felipeLeao18 <flpleao1807@gmail.com>
2023-01-07 13:47:57 -0300

    feat: add signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -5,8 +5,68 @@ import logoImg from '@/public/logo.svg'
 import styles from '@/styles/home.module.scss'
 import { Input } from '@/src/components/ui/Input'
 import { Button } from '@/src/components/ui/Button'
+import { FormEvent, useState } from 'react'
+import { api } from '@/src/services/apiClient'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Router from 'next/router'
+interface ISignUp {
+  email: string
+  name: string
+  password: string
+}
 
 export default function SignUp () {
+  const [loading, setLoading] = useState(false)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+
+  async function handleSubmit (event: FormEvent) {
+    event.preventDefault()
+    const user: ISignUp & { passwordConfirm: string } = {
+      name,
+      email,
+      password,
+      passwordConfirm
+    }
+
+    const allDataProvided = Object.values(user)?.every(value => value)
+    console.log(allDataProvided)
+
+    if (!allDataProvided) {
+      toast.error('All fields are required')
+      return
+    }
+
+    if (user.password !== user.passwordConfirm) {
+      toast.error('Password confirmation failed')
+      return
+    }
+
+    setLoading(true)
+    try {
+      const response = (await api.post('/user',
+        { email: user.email, name: user.name, password: user.password }))
+        .data
+
+      const { user: userSignedUp } = response
+      setLoading(false)
+      if (userSignedUp !== null) {
+        toast.success(`WELCOME ${user.name}`)
+        await Router.push('/')
+      }
+    } catch (err: any) {
+      console.log(err)
+
+      setLoading(false)
+      return toast.error(err.response.data.message)
+    }
+
+    // me ame
+  }
+
   return (
     <>
     <div>
@@ -17,26 +77,29 @@ export default function SignUp () {
         </Head>
     </div>
       <div className={styles.containerCenter}>
-
         <Image src={logoImg} alt="logo"/>
 
         <div className={styles.login}>
 
           <h1>Cadastro</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <Input placeholder="Digite seu nome" type="text"
+              onChange={(e) => setName(e.target.value)}
               />
             <Input placeholder="Digite seu email" type="text"
+              onChange={(e) => setEmail(e.target.value)}
               />
             <Input placeholder="Digite sua senha"
               type="password"
+              onChange={(e) => setPassword(e.target.value)}
               />
             <Input placeholder="confirme sua senha"
               type="password"
+              onChange={(e) => setPasswordConfirm(e.target.value)}
               />
             <Button
               type="submit"
-              loading={false}
+              loading={loading}
             >
             Cadastrar
             </Button>
